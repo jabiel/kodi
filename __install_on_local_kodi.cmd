47f76c6635f0c86b3e4e5e9d53c7a3053859ab0c
jabiel <jabiel@o2.pl>
2018-01-21 10:43:16 +0000

    Solution cleanup

--- __install_on_local_kodi.cmd
+++ __install_on_local_kodi.cmd
@@ -1,5 +1,1 @@
-rem copy plugin.video.hellokoko\default.py C:\Users\%USERNAME%\AppData\Roaming\Kodi\addons\plugin.video.hellokoko
 xcopy /E /Y plugin.video.hellokoko\*.py C:\Users\%USERNAME%\AppData\Roaming\Kodi\addons\plugin.video.hellokoko\
-
-
-pause
